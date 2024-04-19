ca25f5fc1f857b797f03eb93d651afd27ae71233
Cnnb01 <nyamambanyaguch@gmail.com>
2024-04-19 18:43:45 +0300

    install flask from pip3

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-# Creating a file
-file {'/tmp/school':
-    content => 'I love Puppet',
-    group => 'www-data',
-    mode => '0744',
-    owner => 'www-data',
-}
+# This puppet manifest creates a file names/tmp/school
+file { '/tmp/school':
+  content => "I love Puppet\n",
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  }
