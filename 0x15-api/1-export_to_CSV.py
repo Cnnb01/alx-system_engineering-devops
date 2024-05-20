a41eb120fac26b5fda053bf080e7f439fc5bd40b
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-20 21:53:09 +0300

    apis

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
-"""
-This script fetches and displays information about an
-employee's todo list progress as a csv.
-"""
+"""Exports to CSV"""
+
 from requests import get
 from sys import argv
 
-if __name__ == "__main__":
-    users_id = argv[1]
 
-    # fething user's info
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(users_id)
+if __name__ == '__main__':
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     response = get(url)
-    username = response.json().get("username")
+    username = response.json().get('username')
 
-    # fething user's todo list
-    td = "https://jsonplaceholder.typicode.com/users/{}/todos".format(users_id)
-    response = get(td)
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    response = get(url)
     tasks = response.json()
-
-    with open('{}.csv'.format(users_id), 'w') as file:
+    with open('{}.csv'.format(user_id), 'w') as file:
         for task in tasks:
             file.write('"{}","{}","{}","{}"\n'
-                       .format(users_id, username, task.get('completed'),
+                       .format(user_id, username, task.get('completed'),
                                task.get('title')))
