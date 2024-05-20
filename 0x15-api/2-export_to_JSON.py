da7cf8bbde2b0d6a6298ba32cd50b4245b0ec651
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-20 21:56:08 +0300

    apis

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""
-This script fetches and displays information about an
-employee's todo list progress as a json.
-"""
+"""Exports data to JSON format"""
+
 from json import dump
 from requests import get
 from sys import argv
 
-if __name__ == "__main__":
-    users_id = argv[1]
 
-    # fething user's info
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(users_id)
+if __name__ == '__main__':
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     response = get(url)
-    username = response.json().get("username")
+    username = response.json().get('username')
 
-    # fething user's todo list
-    td = "https://jsonplaceholder.typicode.com/users/{}/todos".format(users_id)
-    response = get(td)
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+    response = get(url)
     tasks = response.json()
-    dictionary = {users_id: []}
+    dictionary = {user_id: []}
     for task in tasks:
-        dictionary[users_id].append({
+        dictionary[user_id].append({
                                     "task": task.get('title'),
                                     "completed": task.get('completed'),
                                     "username": username
                                     })
-    with open('{}.json'.format(users_id), 'w') as file:
+    with open('{}.json'.format(user_id), 'w') as file:
         dump(dictionary, file)
