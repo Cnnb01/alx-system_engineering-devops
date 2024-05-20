7af672791e227d4f2299592467e721217e9ecc9d
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-20 20:35:30 +0300

    returns information about users TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script fetches and displays information about an 
+This script fetches and displays information about an
 employee's todo list progress.
 """
 from sys import argv
@@ -11,20 +11,20 @@ if __name__ == "__main__":
     users_id = argv[1]
 
     # fething user's info
-    users_info = requests.get (main_url + "users/{}".format(users_id)).json()
+    users_info = requests.get(main_url + "users/{}".format(users_id)).json()
     user_name = users_info.get("name")
 
     # fething user's todo list
-    users_todos = requests.get (main_url + "todos?userId={}".format(users_id)).json()
+    users_todos = requests.get(main_url + "todos?userId={}"
+                               .format(users_id)).json()
     total_tasks = len(users_todos)
-    
+
     # putting comp tasks in list
-    completed_ts = [ t for t in users_todos if t.get("completed") is True ]
+    completed_ts = [t for t in users_todos if t.get("completed") is True]
     num_completed_ts = len(completed_ts)
 
     print("Employee {} is done with tasks({}/{}):".format(
         user_name, num_completed_ts, total_tasks))
-    
+
     for ts in completed_ts:
         print("\t {}".format(ts.get("title")))
-
