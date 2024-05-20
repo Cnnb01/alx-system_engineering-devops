98ac563a83673d4f44d4ff4cca1f299fb128dadf
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-20 20:32:14 +0300

    returns information about users TODO list progress.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-
+This script fetches and displays information about an 
+employee's todo list progress.
 """
 from sys import argv
 import requests
@@ -8,14 +9,22 @@ import requests
 if __name__ == "__main__":
     main_url = "https://jsonplaceholder.typicode.com/"
     users_id = argv[1]
-    users_id_url = requests.get (url + "users/{}".format(users_id)).json()
-    users_todos = requests.get (url + "todos/userId{}".format(users_id)).json()
+
+    # fething user's info
+    users_info = requests.get (main_url + "users/{}".format(users_id)).json()
+    user_name = users_info.get("name")
+
+    # fething user's todo list
+    users_todos = requests.get (main_url + "todos?userId={}".format(users_id)).json()
     total_tasks = len(users_todos)
-    user_name = users_id.get("name")
-    completed_ts = []
-    for t in users_todos:
-        if t.get("completed") is True:
-            return completed.append(titlee = t.get("title"))
+    
+    # putting comp tasks in list
+    completed_ts = [ t for t in users_todos if t.get("completed") is True ]
+    num_completed_ts = len(completed_ts)
+
     print("Employee {} is done with tasks({}/{}):".format(
-        user_name, completed_ts, total_tasks))
+        user_name, num_completed_ts, total_tasks))
+    
+    for ts in completed_ts:
+        print("\t {}".format(ts.get("title")))
 
